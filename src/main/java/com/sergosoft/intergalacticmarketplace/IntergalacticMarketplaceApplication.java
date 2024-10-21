f695f3c0e9410d29ff42edf65352c1e6815e1782
sergiorbk <sergey24rybak@gmail.com>
2024-10-21 22:00:30 +0300

    Basic project structure was implemented

---
+++ src/main/java/com/sergosoft/intergalacticmarketplace/IntergalacticMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.sergosoft.intergalacticmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergalacticMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergalacticMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergosoft/intergalacticmarketplace/IntergalacticMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergosoft.intergalacticmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntergalacticMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
